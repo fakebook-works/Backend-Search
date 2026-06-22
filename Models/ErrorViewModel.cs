a3a8933b650da07266f1d69e76feb09e91c65c43
Trần Lê Minh <45527810+leminh249@users.noreply.github.com>
2026-06-22 11:03:00 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend_Search_Fakebook.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
