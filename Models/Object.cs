8a1971134695d3a421075b333db03ca9d67cc145
Trần Lê Minh <45527810+leminh249@users.noreply.github.com>
2026-06-22 10:53:15 +0700

    Models

---
+++ Models/Object.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend_Search_Fakebook.Models;
+
+public partial class Object
+{
+    public long Id { get; set; }
+
+    public string Type { get; set; } = null!;
+
+    public int? SortKey { get; set; }
+
+    public long OwnerId { get; set; }
+
+    public int? PrivacyLevel { get; set; }
+
+    public virtual ICollection<Token> Tokens { get; set; } = new List<Token>();
+}
